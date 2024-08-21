822eed2145950dfe6c79854babcc772a7b96ca28
DDH2808 <daodinhhoan2002@gmail.com>
2024-08-21 10:57:31 +0700

    [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,17 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
 
     // [GET] /
-    async index(req, res) {
-        await Course.find({})
-            .then((courses) => res.json(courses))
-            .catch((err) => res.status(400).json({ error: 'ERROR!!!' }))
-        // res.render('home');
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
     }
 
     // [GET] /search
