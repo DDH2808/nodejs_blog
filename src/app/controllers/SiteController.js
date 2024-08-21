5e544e09287d45cc7c691070ab2c5a08a7bc1dbd
DDH2808 <daodinhhoan2002@gmail.com>
2024-08-21 10:28:41 +0700

    [MVC] Model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,13 @@
+const Course = require('../models/Course');
+
 class SiteController {
 
     // [GET] /
-    index(req, res) {
-        res.render('home');
+    async index(req, res) {
+        await Course.find({})
+            .then((courses) => res.json(courses))
+            .catch((err) => res.status(400).json({ error: 'ERROR!!!' }))
+        // res.render('home');
     }
 
     // [GET] /search
